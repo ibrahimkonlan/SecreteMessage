cd2b5121d6a9f4bbc920d5c4b9e6c1462fdf0932
ibrahimkonlan <haruna.konlan@amalitech.org>
2021-11-27 19:14:23 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'program');
+
+secretMessage[7] = 'right';
+
+//console.log(secretMessage);
+
+secretMessage.shift();
+//console.log(secretMessage);
+
+secretMessage.unshift();
+
+//console.log(secretMessage);
+
+secretMessage.shift[6, 5, "know,"];
+
+console.log(secretMessage.join(' '));
+
+
